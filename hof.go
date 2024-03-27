4af6c64b3da7f9b4a52e6b335fa7b7a01f45604b
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-03-28 05:39:00 +0600

    add doces and change the map function api

--- hof.go
+++ hof.go
@@ -1,7 +1,16 @@
+// This package provides higher-order functions for working with slices.
 package hof
 
 import "errors"
 
+// ForEach applies a function to each element of a slice.
+//
+// The function callback is called with the index of the element,
+// the element itself, and the entire slice.
+//
+// If slice is nil, it returns an error.
+//
+// If callback is nil, it returns an error.
 func ForEach[T any](slice []T, callback func(int, T, []T)) error {
 	if slice == nil {
 		return errors.New("slice is nil")
@@ -9,23 +18,31 @@ func ForEach[T any](slice []T, callback func(int, T, []T)) error {
 	if callback == nil {
 		return errors.New("callback is nil")
 	}
-	for i, item := range slice {
-		callback(i, item, slice)
+	for index, item := range slice {
+		callback(index, item, slice)
 	}
 	return nil
 }
 
-func Map[T, U any](slice []T, callback func(T) U) ([]U, error) {
+// Map applies a function to each element of a slice and returns a new slice
+// containing the results of applying the function to each element.
+//
+// The function callback is called with the index of the element,
+// the element itself, and the entire slice.
+//
+// If slice is nil, it returns nil and an error.
+//
+// If callback is nil, it returns nil and an error.
+func Map[T, U any](slice []T, callback func(int, T, []T) U) ([]U, error) {
 	if slice == nil {
 		return nil, errors.New("slice is nil")
 	}
 	if callback == nil {
 		return nil, errors.New("callback is nil")
 	}
-
 	mapped := make([]U, len(slice))
-	for i, v := range slice {
-		mapped[i] = callback(v)
+	for index, item := range slice {
+		mapped[index] = callback(index, item, slice)
 	}
 	return mapped, nil
 }
--- map_test.go
+++ map_test.go
@@ -8,21 +8,30 @@ import (
 	"github.com/p-nerd/hof"
 )
 
-func TestMapInt(t *testing.T) {
+func TestMapIntSlice(t *testing.T) {
 	input := []int{1, 2, 3, 4, 5}
 	expected := []int{2, 4, 6, 8, 10}
-	result, _ := hof.Map(input, func(x int) int { return x * 2 })
+	result, err := hof.Map(input, func(_ int, item int, _ []int) int { return item * 2 })
+	if err != nil {
+		t.Errorf("Error occurred during map operation: %v", err)
+	}
 	if !reflect.DeepEqual(result, expected) {
 		t.Errorf("Test for integers failed: expected %v, got %v", expected, result)
 	}
 }
 
-func TestMapString(t *testing.T) {
-	inputStr := []string{"hello", "world"}
-	expectedStr := []string{"HELLO", "WORLD"}
-	resultStr, _ := hof.Map(inputStr, func(s string) string { return strings.ToUpper(s) })
-	if !reflect.DeepEqual(resultStr, expectedStr) {
-		t.Errorf("Test for strings failed: expected %v, got %v", expectedStr, resultStr)
+func TestMapStringSlice(t *testing.T) {
+	input := []string{"hello", "world"}
+	expected := []string{"HELLO", "WORLD"}
+	result, err := hof.Map(
+		input,
+		func(_ int, item string, _ []string) string { return strings.ToUpper(item) },
+	)
+	if err != nil {
+		t.Errorf("Error occurred during map operation: %v", err)
+	}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Test for integers failed: expected %v, got %v", expected, result)
 	}
 }
 
@@ -38,7 +47,10 @@ func TestMapStruct(t *testing.T) {
 		{ID: 3, Name: "Charlie"},
 	}
 	expectedIDs := []int{1, 2, 3}
-	resultIDs, _ := hof.Map(users, func(user User) int { return user.ID })
+	resultIDs, err := hof.Map(users, func(_ int, user User, _ []User) int { return user.ID })
+	if err != nil {
+		t.Errorf("Error occurred during map operation: %v", err)
+	}
 	if !reflect.DeepEqual(resultIDs, expectedIDs) {
 		t.Errorf("Test for structs failed: expected %v, got %v", expectedIDs, resultIDs)
 	}
